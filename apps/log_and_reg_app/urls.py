181fd2bd3cbd0870a26138591c92e245e5db3206
Promise Izuchukwu <okoroaforizuchukwu@gmail.com>
2024-08-21 03:22:12 -1100

    first full commit after long works

---
+++ apps/log_and_reg_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    url(r'^$', views.index),
+    url(r'^success$', views.success),
+
+    url(r'^user/register$', views.register),
+    url(r'^user/login$', views.login),
+
+    url(r'^logout$', views.reset),
+    ]
